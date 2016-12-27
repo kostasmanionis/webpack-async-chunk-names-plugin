1d4b20e34c4d37df31f34602a090b1ab7fa36109
Kostas Manionis <kostas@1stdibs.com>
2016-12-27 14:45:34 +0200

    Detect AsyncDependenciesBlock
    Try to figure out when we're dealing with async dependencies. Smells a bit, but hey... it works.

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const webpack = require('webpack')
 const path = require('path')
+const AsyncDependenciesBlock = require('webpack/lib/AsyncDependenciesBlock');
+const DEP_BLOCK_NAME = 'AsyncDependenciesBlock';
 
-function AsyncChunkNames() {
-
+function checkConstructorNames(object) {
+    const obj = Object.getPrototypeOf(object);
+    if(obj) {
+        if (obj.constructor.name === DEP_BLOCK_NAME) {
+            return true;
+        } else {
+            return checkConstructorNames(obj);
+        }
+    } else {
+        return false;
+    }
 }
+
+function AsyncChunkNames() {}
 
 AsyncChunkNames.prototype.apply = function (compiler) {
     compiler.plugin('compilation', function (compilation) {
         compilation.plugin('seal', function () {
             compilation.modules.forEach(function (module) {
                 module.blocks.forEach(function (block) {
-                    block.dependencies.forEach(function (dependency) {
-                        const parsedPath = path.parse(dependency.module.resource);
-                        dependency.block.chunkName = parsedPath.name;
-                        dependency.block.name = parsedPath.name;
-                    });
+                    if (checkConstructorNames(block)) {
+                        block.dependencies.forEach(function (dependency) {
+                            const parsedPath = path.parse(dependency.module.resource);
+                            dependency.block.chunkName = parsedPath.name;
+                            dependency.block.name = parsedPath.name;
+                        });
+                    }
                 });
             });
         });
